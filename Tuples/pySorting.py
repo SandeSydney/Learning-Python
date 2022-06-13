6418a3cf2821dd39abdf2ffa52932c6cfd03291e
SandeSydney <sandesydney01@gmail.com>
2022-06-13 08:36:50 +0300

    Sorted data

---
+++ Tuples/pySorting.py
@@ -0,0 +1,9 @@
+## This python script uses the sorting functionality of tuples
+
+# create a dictionary of items
+d = {'u':25, 'n':15, 'g':12, 'w': 10}
+print(d.items())
+
+## sort the dictionary with the sorted function using the items() method
+print('Sorted data')
+print(sorted(d.items()))
